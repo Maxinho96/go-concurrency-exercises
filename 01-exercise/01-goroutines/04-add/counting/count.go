535a80cd8e068f5a723d49828f9b29cb7d4f1341
Massimiliano Bruni <massimiliano.bruni@outfittery.de>
2023-05-19 16:20:23 +0200

    exercises 3, 4 and 5

--- 01-exercise/01-goroutines/04-add/counting/count.go
+++ 01-exercise/01-goroutines/04-add/counting/count.go
@@ -1,7 +1,10 @@
 package counting
 
 import (
+	"math"
 	"math/rand"
+	"runtime"
+	"sync"
 	"time"
 )
 
@@ -34,12 +37,31 @@ func Add(numbers []int) int64 {
 func AddConcurrent(numbers []int) int64 {
 	var sum int64
 	// Utilize all cores on machine
-
+	cores := runtime.NumCPU()
 	// Divide the input into parts
-
+	var parts [][]int
+	slice := int(math.Ceil(float64(len(numbers)) / float64(cores)))
+	for i := 0; i < len(numbers); i += slice {
+		end := i + slice
+		if end > len(numbers) {
+			end = len(numbers)
+		}
+		parts = append(parts, numbers[i:end])
+	}
 	// Run computation for each part in seperate goroutine.
-
+	var wg sync.WaitGroup
+	wg.Add(len(parts))
+	sums := make([]int64, len(parts))
+	for i, part := range parts {
+		go func(part []int, i int) {
+			defer wg.Done()
+			sums[i] = Add(part)
+		}(part, i)
+	}
+	wg.Wait()
 	// Add part sum to cummulative sum
-
+	for _, partsum := range sums {
+		sum += partsum
+	}
 	return sum
 }
