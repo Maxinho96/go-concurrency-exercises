08d05354e61541be52c68d6dcc7df8ee070908ea
Massimiliano Bruni <massimiliano.bruni@outfittery.de>
2023-06-30 16:48:25 +0200

    pipeline remaining exercises

--- 02-exercise/02-pipeline/02-pipeline/main.go
+++ 02-exercise/02-pipeline/02-pipeline/main.go
@@ -2,6 +2,11 @@
 
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func generator(nums ...int) <-chan int {
 	out := make(chan int)
 
@@ -28,13 +33,34 @@ func square(in <-chan int) <-chan int {
 func merge(cs ...<-chan int) <-chan int {
 	// Implement fan-in
 	// merge a list of channels to a single channel
+	out := make(chan int)
+	var wg sync.WaitGroup
+	for _, ch := range cs {
+		wg.Add(1)
+		go func(ch <-chan int) {
+			defer wg.Done()
+			for n := range ch {
+				out <- n
+			}
+		}(ch)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
 
 func main() {
 	in := generator(2, 3)
 
 	// TODO: fan out square stage to run two instances.
+	out1 := square(in)
+	out2 := square(in)
 
 	// TODO: fan in the results of square stages.
-
+	out := merge(out1, out2)
+	for i := range out {
+		fmt.Println(i)
+	}
 }
